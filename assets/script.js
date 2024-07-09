fe374cdfe8b41881ace5a35eb2cb832b79e69215
Vijay pradeep <vijaypradeep3568@gmail.com>
2024-07-09 18:08:58 +0530

    add script for image slider

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
-const container = document.querySelectorAll( ".containerslide");
-Counter=0;
-container.forEach (
-    (containerslide,index)=> {
-        containerslide.style.left=`${index*100}%` 
-    }
-);
+var subcontainer=document.querySelector('.container');
+const prev= document.getElementById('prev');
+const next=document.getElementById('next');
+var itemwidith=500;
 
+var padding =10;
+prev.addEventListener("click",() =>
+    {
+        subcontainer.scrollLeft-=itemwidith+padding;
+    })
+    next.addEventListener("click",() =>
+    {
+        subcontainer.scrollLeft+=itemwidith+padding;
+    })
+
+var prevs = document.getElementById('prevs');
+var nexts =document.getElementById('nexts');
+var container=document.querySelector('.container2');
+var cards=container.querySelectorAll('.cards');
+
+prevs.addEventListener('click',() => {
+    container.scrollLeft-=itemwidith+padding;
+
+
+});
+ nexts.addEventListener("click" ,() =>
+{
+    container.scrollLeft+=itemwidith+padding;
+});
